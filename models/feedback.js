72c621e76d35848135490c55b890c9306e72f6b8
Mohammad Pourcheriki <Pourcheriki@gmail.com>
2018-11-06 17:00:15 +0330

    ADD Mongoose Fix Form Save Feedbacks in Mongodb

--- models/feedback.js
+++ models/feedback.js
@@ -1,13 +1,14 @@
-const mongoose = require('mongoose');
-const Schema = mongoose.Schema;
+let mongoose = require('mongoose');
+let Schema = mongoose.Schema;
 
 let FeedbackSchema = new Schema({
     lname: {type:String,required:true},
     tel: {type:String},
     email: {type:String},
     file_url: {type : String},
-    desc: {type:String}
+    desc: {type:String},
+    date: Date
 });
 
 // Export model.
-module.exports = mongoose.model('FeedBack', FeedbackSchema);
+module.exports = mongoose.model('Feedback', FeedbackSchema);
--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,7 @@
 var express = require('express');
+const Feedback = require('../models/feedback');
 var router = express.Router();
+
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
@@ -8,6 +10,18 @@ router.get('/', function(req, res, next) {
 
 /* POST Upload EndPoint. */
 router.post('/upload', function(req, res, next) {
+  console.log(req.body);
+  let date = new Date(Date.now());
+  let feedback = new Feedback({
+    lname: req.body.lanme,
+    tel: req.body.tel,
+    email: req.body.email,
+    desc: req.body.comment,
+    date: date
+  });
+  feedback.save(function (err) {
+    console.log(err);
+  });
   res.send('Thanks!');
 });
 
