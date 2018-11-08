046c9ff1dbc5a9d410872100562afc9ee5029564
Mohammad Pourcheriki <Pourcheriki@gmail.com>
2018-11-08 15:07:48 +0330

    Initial passport module

--- app.js
+++ app.js
@@ -3,7 +3,10 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+const favicon = require('serve-favicon');
 var mongoose = require('mongoose');
+const passport = require('passport');
+const session = require('express-session');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
@@ -16,11 +19,21 @@ mongoose.connect('mongodb://localhost:27017/feedbacksindad', { useNewUrlParser: 
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
+app.use(favicon(__dirname + '/public/favicon.ico'));
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+
+app.use(session({
+  secret: 'ertcXkUAAA[asd}sfghxllUedOsgfhgfz8',
+  resave: true,
+  saveUninitialized: true, 
+}));
+
+app.use(passport.initialize());
+app.use(passport.session());
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
